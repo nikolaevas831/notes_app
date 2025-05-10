15b862b5fe1d2a07a305e227d397d0924af42e88
Artem Nikolaev <nikolaevas831@gmail.com>
2025-05-10 14:50:11 +0300

    Remove dotenv

--- src/notes_app/database.py
+++ src/notes_app/database.py
@@ -1,12 +1,10 @@
 import os
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine, Integer, String, select
 from sqlalchemy.orm import (
     sessionmaker, declarative_base, Mapped, mapped_column, Session
 )
 
-load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
