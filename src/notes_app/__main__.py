d29a84b27a9583c9043cc86765dd87bda7343959
Artem Nikolaev <nikolaevas831@gmail.com>
2025-05-10 23:55:11 +0300

    Remove model_validate methods

--- src/notes_app/__main__.py
+++ src/notes_app/__main__.py
@@ -6,31 +6,40 @@ from notes_app.schemas import Note as NotePydantic
 
 app = typer.Typer()
 
+
 @app.command()
 def create_note(head: str, body: str):
     with current_session() as session:
-        note = database.create_note(db=session, head=head, body=body)
+        note_repo = NoteRepo(session)
+        note = Note(head=head, body=body)
+        note_repo.add_note(note)
+        session.commit()
         print(note)
 
 
 @app.command()
 def read_note(note_id: int):
     with current_session() as session:
-        note = database.get_note(db=session, note_id=note_id)
+        note_repo = NoteRepo(session)
+        note = note_repo.get_note(note_id=note_id)
         if not note:
             print("Заметка не найдена")
         else:
-            validate_note = NotePydantic.model_validate(note)
-            print(validate_note.model_dump())
+            note = NotePydantic(id=note.id, head=note.head,
+                                body=note.body).model_dump()
+            print(note)
 
 
 @app.command()
 def list_notes():
     with current_session() as session:
-        notes = database.get_notes(db=session)
+        note_repo = NoteRepo(session)
+        notes = note_repo.get_notes()
+        print(notes)
         for note in notes:
-            validate_note = NotePydantic.model_validate(note)
-            print(validate_note.model_dump())
+            note = NotePydantic(id=note.id, head=note.head,
+                                body=note.body).model_dump()
+            print(note)
 
 
 if __name__ == "__main__":
