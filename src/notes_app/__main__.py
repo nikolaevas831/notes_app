51fc0591407b64c9b54951f4c2acf81ce329aa10
Artem Nikolaev <nikolaevas831@gmail.com>
2025-05-14 12:19:29 +0300

    Replace Typer app with FastAPI app

--- src/notes_app/__main__.py
+++ src/notes_app/__main__.py
@@ -1,44 +1,53 @@
-import typer
+from fastapi import FastAPI, HTTPException
 
 from notes_app.database import current_session, NoteRepo, Note
 from notes_app.schemas import NotePydantic
 
-app = typer.Typer()
+app = FastAPI()
 
 
-@app.command()
+@app.post("/create-note")
 def create_note(head: str, body: str):
     with current_session() as session:
         note_repo = NoteRepo(session)
         note = Note(head=head, body=body)
         note_repo.add_note(note)
         session.commit()
-        print(note)
 
 
-@app.command()
+@app.post("/delete-note")
+def delete_note(note_id):
+    with current_session() as session:
+        note_repo = NoteRepo(session)
+        note_repo.delete_note(note_id=note_id)
+        session.commit()
+
+
+@app.get("/read-note")
 def read_note(note_id: int):
     with current_session() as session:
         note_repo = NoteRepo(session)
         note = note_repo.get_note(note_id=note_id)
         if not note:
-            print("Заметка не найдена")
+            raise HTTPException(status_code=404, detail="Заметка не найдена")
         else:
             note = NotePydantic(id=note.id, head=note.head,
                                 body=note.body).model_dump()
-            print(note)
+            return note
 
 
-@app.command()
+@app.get("/list-notes")
 def list_notes():
     with current_session() as session:
         note_repo = NoteRepo(session)
         notes = note_repo.get_notes()
-        print(notes)
-        for note in notes:
-            note = NotePydantic(id=note.id, head=note.head,
-                                body=note.body).model_dump()
-            print(note)
+        if not notes:
+            raise HTTPException(status_code=404, detail="Заметки не найдены")
+        else:
+            pydantic_notes = [
+                NotePydantic(id=n.id, head=n.head, body=n.body).model_dump()
+                for n in notes]
+            return pydantic_notes
 
 
 if __name__ == "__main__":
