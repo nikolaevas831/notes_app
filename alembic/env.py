bcf42d7349a5402613d70002a8114faa34647b59
Artem Nikolaev <nikolaevas831@gmail.com>
2025-08-10 11:57:27 +0300

    Update migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from src.notes_app.database import Base
+from src.notes_app.infrastructure.database.main import Base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("MIGRATION_DATABASE_URL")
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -43,9 +43,12 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata,
-                      literal_binds=True,
-                      dialect_opts={"paramstyle": "named"}, )
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
 
     with context.begin_transaction():
         context.run_migrations()
@@ -60,11 +63,12 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.", poolclass=pool.NullPool, )
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection,
-                          target_metadata=target_metadata)
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- alembic/versions/488beed45453_create_users_and_notes_tables.py
+++ alembic/versions/488beed45453_create_users_and_notes_tables.py
@@ -1,10 +1,11 @@
 """Create users and notes tables
 
 Revision ID: 488beed45453
-Revises: 
+Revises:
 Create Date: 2025-05-14 22:42:09.474232
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '488beed45453'
+revision: str = "488beed45453"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -20,24 +21,29 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.create_table('users',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('username', sa.String(), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('username')
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("username", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("username"),
     )
-    op.create_table('notes',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('head', sa.String(), nullable=False),
-    sa.Column('body', sa.String(), nullable=True),
-    sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
-    sa.PrimaryKeyConstraint('id')
+    op.create_table(
+        "notes",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("head", sa.String(), nullable=False),
+        sa.Column("body", sa.String(), nullable=True),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
     )
+
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.drop_table('notes')
-    op.drop_table('users')
-
+    op.drop_table("notes")
+    op.drop_table("users")
