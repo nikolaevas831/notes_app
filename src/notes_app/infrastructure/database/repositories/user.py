a2d4a4576c49af37f11c4cef9730ca738435a2bd
nikolaevas <pass>
2025-12-30 12:16:33 +0300

    refactor(infrastructure): add mappers to repository classes initialization

--- src/notes_app/infrastructure/database/repositories/user.py
+++ src/notes_app/infrastructure/database/repositories/user.py
@@ -4,34 +4,35 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from notes_app.application.interfaces.user_repo import UserRepoInterface
 from notes_app.domain.entities.user import User as UserEntity
 from notes_app.infrastructure.database.mappers.user import UserMapper
-from notes_app.infrastructure.database.models.user import User
+from notes_app.infrastructure.database.models.user import User as UserORM
 
 
 class UserRepo(UserRepoInterface):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
+        self._mapper = UserMapper()
 
-    async def add_user(self, user: UserEntity) -> User | None:
-        user_orm = UserMapper.map_user_entity_to_orm(user)
+    async def add_user(self, user: UserEntity) -> UserEntity | None:
+        user_orm = self._mapper.map_user_entity_to_orm(user)
         self._session.add(user_orm)
         await self._session.flush()
         await self._session.refresh(user_orm)
-        return UserMapper.map_user_orm_to_entity(user_orm)
+        return self._mapper.map_user_orm_to_entity(user_orm)
 
     async def get_user_by_user_id(self, user_id: int) -> UserEntity | None:
-        stmt = select(User).where(User.id == user_id)
+        stmt = select(UserORM).where(UserORM.id == user_id)
         result = await self._session.scalars(stmt)
         user_orm = result.first()
         if not user_orm:
             return None
-        user_entity = UserMapper.map_user_orm_to_entity(user_orm)
+        user_entity = self._mapper.map_user_orm_to_entity(user_orm)
         return user_entity
 
     async def get_user_by_username(self, username: str) -> UserEntity | None:
-        stmt = select(User).where(User.username == username)
+        stmt = select(UserORM).where(UserORM.username == username)
         result = await self._session.scalars(stmt)
         user_orm = result.first()
         if not user_orm:
             return None
-        user_entity = UserMapper.map_user_orm_to_entity(user_orm)
+        user_entity = self._mapper.map_user_orm_to_entity(user_orm)
         return user_entity
