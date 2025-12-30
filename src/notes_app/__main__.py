9be0f47b0f401a750af63b85c3707d48004a2d7b
nikolaevas <pass>
2025-12-30 13:29:48 +0300

    refactor(app): migrate to centralized configuration and extend lifespan

--- src/notes_app/__main__.py
+++ src/notes_app/__main__.py
@@ -1,4 +1,3 @@
-import os
 from contextlib import asynccontextmanager
 
 import uvicorn
@@ -7,30 +6,37 @@ from fastapi import FastAPI
 
 from notes_app.api.routers.auth import router as auth_router
 from notes_app.api.routers.note import router as note_router
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    producer = AIOKafkaProducer(bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"))
-    await producer.start()
-    app.state.kafka_producer = producer
-    try:
-        yield
-    finally:
-        await producer.stop()
-
-
-app = FastAPI(lifespan=lifespan)
-app.include_router(auth_router)
-app.include_router(note_router)
+from notes_app.infrastructure.auth.jwt_token_service import JwtTokenService
+from notes_app.infrastructure.config import load_config
+from notes_app.infrastructure.database.main import build_async_engine, build_async_session_factory
 
 
 def main():
+    config = load_config()
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        producer = AIOKafkaProducer(bootstrap_servers=config.notifier.bootstrap_servers)
+        db_engine = build_async_engine(db_config=config.db)
+        db_session_factory = build_async_session_factory(engine=db_engine)
+        jwt_token_service = JwtTokenService(auth_config=config.auth)
+        await producer.start()
+        app.state.kafka_producer = producer
+        app.state.db_engine = db_engine
+        app.state.db_session_factory = db_session_factory
+        app.state.jwt_service = jwt_token_service
+        try:
+            yield
+        finally:
+            await producer.stop()
+
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(auth_router)
+    app.include_router(note_router)
     uvicorn.run(
-        "notes_app.__main__:app",
-        host=os.getenv("UVICORN_FASTAPI_HOST"),
-        port=int(os.getenv("UVICORN_FASTAPI_PORT")),
-        reload=True,
+        app=app,
+        host=config.api.host,
+        port=config.api.port,
+        reload=False,
     )
 
 
