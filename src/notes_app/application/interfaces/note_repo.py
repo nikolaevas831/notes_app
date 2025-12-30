4fcf05b41174252627aa905f677351a3fea27b3b
nikolaevas <pass>
2025-12-30 13:33:17 +0300

    refactor(application): update interface function return type annotations

--- src/notes_app/application/interfaces/note_repo.py
+++ src/notes_app/application/interfaces/note_repo.py
@@ -1,24 +1,24 @@
 import abc
 
-from notes_app.application.dto.note import CreateNoteDTO
-from notes_app.domain.entities.note import Note
+from notes_app.application.dto.note import CreateNoteDTO, NoteDTO
+from notes_app.domain.entities.note import Note as NoteEntity
 
 
 class NoteRepoInterface(abc.ABC):
     @abc.abstractmethod
-    async def add_note(self, note: CreateNoteDTO) -> Note:
+    async def add_note(self, note: CreateNoteDTO) -> NoteDTO:
         pass
 
     @abc.abstractmethod
-    async def get_note(self, note_id: int) -> Note | None:
+    async def get_note(self, note_id: int) -> NoteDTO | None:
         pass
 
     @abc.abstractmethod
-    async def get_notes(self, user_id: int) -> list[Note]:
+    async def get_notes(self, user_id: int) -> list[NoteDTO]:
         pass
 
     @abc.abstractmethod
-    async def delete_note(self, note_id: int) -> Note | None:
+    async def delete_note(self, note_id: int) -> NoteDTO | None:
         pass
 
 
--- src/notes_app/application/interfaces/user_repo.py
+++ src/notes_app/application/interfaces/user_repo.py
@@ -1,17 +1,17 @@
 import abc
 
-from notes_app.domain.entities.user import User
+from notes_app.domain.entities.user import User as UserEntity
 
 
 class UserRepoInterface(abc.ABC):
     @abc.abstractmethod
-    async def add_user(self, user: User) -> User:
+    async def add_user(self, user: UserEntity) -> UserEntity:
         pass
 
     @abc.abstractmethod
-    async def get_user_by_user_id(self, user_id: int) -> User | None:
+    async def get_user_by_user_id(self, user_id: int) -> UserEntity | None:
         pass
 
     @abc.abstractmethod
-    async def get_user_by_username(self, username: str) -> User | None:
+    async def get_user_by_username(self, username: str) -> UserEntity | None:
         pass
