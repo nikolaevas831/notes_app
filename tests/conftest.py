dec0bcef0aab5d70c5b38c19bc6e84c2fbf72ada
nikolaevas <pass>
2026-01-20 13:40:14 +0300

    feat(tests): add conftest.py to register pytest plugins

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+pytest_plugins = (
+    "tests.fixtures.faker",
+    "tests.fixtures.db",
+    "tests.fixtures.app",
+    "tests.fixtures.client",
+    "tests.fixtures.config",
+    "tests.fixtures.user",
+)
